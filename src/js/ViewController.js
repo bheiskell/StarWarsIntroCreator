61cd42303ac204092a3d5c7dc990319fd5a4a2ac
Ben Heiskell <ben@xdxa.org>
2020-11-28 18:49:11 -0500

    Update ViewController.js

--- src/js/ViewController.js
+++ src/js/ViewController.js
@@ -103,7 +103,16 @@ class ViewController {
         this.requestInteractionButton.removeEventListener('click', listener, true);
         resolve();
       });
-      //setTimeout(function() { this.requestInteractionButton.removeEventListener('click', listener, true); resolve(); }, 1000);
+      setTimeout(function() { 
+        var evt = new MouseEvent("click", {
+          view: window,
+          bubbles: true,
+          cancelable: true,
+          clientX: 20,
+          clientY: 20,
+        });
+        targetElement.dispatchEvent(evt);
+      }, 1000);
     });
   }
 
